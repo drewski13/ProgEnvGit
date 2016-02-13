dac8be20b0b39896c547b76735d78a46e420b20c
Shane Andrews <shane.h.andrews@gmail.com>
2016-02-12 21:37:43 -0500

    2nd commit for project with additional info of why

--- myFirstPyDevHW/HelloWorld.py
+++ myFirstPyDevHW/HelloWorld.py
@@ -2,4 +2,10 @@
 
 #My name is Shane Andrews
 
+##This is for a project in my Linux Development Tools class at USF. 
+#If you are reading this then it must have worked and i am pumped that with
+#eclipse it is even easier to use version control! 
+
+ 
+
 print "hello world!";
